0922736162b86e14543e39812413e6a39e98305a
Ariel Rodriguez <ariel@volonbolon.net>
2015-02-16 15:25:35 -0300

    Add Xcode Project

---
+++ ScalingOctoBatman/ScalingOctoBatman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScalingOctoBatman
+//
+//  Created by Ariel Rodriguez on 2/16/15.
+//  Copyright (c) 2015 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
